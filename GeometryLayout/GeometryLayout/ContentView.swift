097cbd6e9f92c5c4ebbfbca4e607554407d803aa
ajtrumbly <ajtrumbly@hotmail.com>
2024-05-22 12:27:27 -0700

    Finished Geometry Layout
    using this project just to test some features and options in swiftui

---
+++ GeometryLayout/GeometryLayout/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  GeometryLayout
+//
+//  Created by Austin Trumbly on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+   var body: some View {
+       GeometryReader { fullView in
+           ScrollView(.vertical) {
+               ForEach(0..<50) { index in
+                   GeometryReader { proxy in
+                       Text("Row #\(index)")
+                           .font(.title)
+                           .frame(maxWidth: .infinity)
+                           .background(Color(hue: min(1, proxy.frame(in: .global).minY / fullView.size.height), saturation: 1, brightness: 1))
+                           .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                           .opacity(proxy.frame(in: .global).minY / 300)
+                           .scaleEffect(max(0.5, proxy.frame(in: .global).minY / 400))
+                   }
+                   .frame(height: 40)
+               }
+           }
+       }
+   }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeometryLayout/GeometryLayout/GeometryLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryLayoutApp.swift
+//  GeometryLayout
+//
+//  Created by Austin Trumbly on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GeometryLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
