cb580b7728312cfad347428b5ea2ddb197df8115
ajtrumbly <ajtrumbly@hotmail.com>
2024-05-22 18:11:41 -0700

    High Rollers complete
    A dice rolling app with a history of previous rolls

---
+++ HighRollers/HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Austin Trumbly on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
