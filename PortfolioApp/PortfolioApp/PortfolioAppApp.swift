c6e1d375ab9deef9dd8249f106e1cf4f16d53d40
ajtrumbly <ajtrumbly@hotmail.com>
2024-06-03 18:22:36 -0700

    Finished SnowSeeker and Started Portfolio App

---
+++ PortfolioApp/PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Austin Trumbly on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
