c4229a88f92668926736068273f910df4fcca54a
ajtrumbly <ajtrumbly@hotmail.com>
2024-06-04 12:07:31 -0700

    CoreData/Layout Structure complete

---
+++ PortfolioApp/PortfolioApp/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  PortfolioApp
+//
+//  Created by Austin Trumbly on 6/3/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ PortfolioApp/PortfolioApp/SidebarView.swift
@@ -0,0 +1,54 @@
+//
+//  SidebarView.swift
+//  PortfolioApp
+//
+//  Created by Austin Trumbly on 6/3/24.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    @EnvironmentObject var dataController: DataController
+    let smartFilter: [Filter] = [.all, .recent]
+    
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
+    
+    var tagFilter: [Filter] {
+        tags.map { tag in
+            Filter(id: tag.id ?? UUID(), name: tag.name ?? "No name", icon: "tag", tag: tag)
+        }
+    }
+    
+    var body: some View {
+        List(selection: $dataController.selectedFilter) {
+            Section("Smart Filters") {
+                ForEach(smartFilter) { filter in
+                    NavigationLink(value: filter) {
+                        Label(filter.name, systemImage: filter.icon)
+                    }
+                }
+            }
+            
+            Section("Tags") {
+                ForEach(tagFilter) { filter in
+                    NavigationLink(value: filter) {
+                        Label(filter.name, systemImage: filter.icon)
+                    }
+                }
+            }
+        }
+        .toolbar {
+            Button {
+                dataController.deleteAll()
+                dataController.createSampleData()
+            } label: {
+                Label("Add Samples", systemImage: "flame")
+            }
+        }
+    }
+}
+
+#Preview {
+    SidebarView()
+        .environmentObject(DataController.preview)
+}
