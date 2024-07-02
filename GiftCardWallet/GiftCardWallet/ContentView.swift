9c493c301306a6f748d8000afff3e91e3bb70b36
ajtrumbly <ajtrumbly@hotmail.com>
2024-07-02 12:26:26 -0700

    Created CRUD for swiftData. Added EditGiftCardView

--- GiftCardWallet/GiftCardWallet/ContentView.swift
+++ GiftCardWallet/GiftCardWallet/ContentView.swift
@@ -9,17 +9,26 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    // Injecting the swiftData container into the view
     @Environment(\.modelContext) var modelContext
     
+    // Creating the wallet query
     @Query var wallet: [GiftCard]
     
     var body: some View {
         NavigationStack {
-            List(wallet) { giftCard in
-                Text(giftCard.store)
+            // Iterate through the query to list each gift card
+            // TODO: Format the cards like the wallet app
+            List {
+                ForEach(wallet) { giftCard in
+                    Text(giftCard.store)
+                }
+                .onDelete(perform: removeGiftCard)
             }
             .navigationTitle("Wallet")
             .toolbar {
+                // Add a new gift card
+                // TODO: Create NewGiftCardView
                 Button(action: addGiftCard) {
                     Label("Add Gift Card", systemImage: "plus")
                 }
@@ -27,10 +36,19 @@ struct ContentView: View {
         }
     }
     
+    // TODO: Move these into a MVVM structure
+    // CRUD Functions
     func addGiftCard() {
         let newCard = GiftCard(store: "Scheels", balance: 100.00)
         
         modelContext.insert(newCard)
+    }
+    
+    func removeGiftCard(at offsets: IndexSet) {
+        for offset in offsets {
+            let giftCard = wallet[offset]
+            modelContext.delete(giftCard)
+        }
     }
 }
 
---
+++ GiftCardWallet/GiftCardWallet/EditGiftCardView.swift
@@ -0,0 +1,21 @@
+//
+//  EditGiftCardView.swift
+//  GiftCardWallet
+//
+//  Created by Austin Trumbly on 7/2/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct EditGiftCardView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditGiftCardView()
+}
