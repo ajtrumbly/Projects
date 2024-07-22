c8dfc3d6fc74f103cb3f97b05c62989d403eb607
ajtrumbly <ajtrumbly@hotmail.com>
2024-07-22 12:21:16 -0700

    Updated Portfolio App for spotlight search

--- PortfolioApp/PortfolioApp/PortfolioAppApp.swift
+++ PortfolioApp/PortfolioApp/PortfolioAppApp.swift
@@ -5,6 +5,7 @@
 //  Created by Austin Trumbly on 6/3/24.
 //
 
+import CoreSpotlight
 import SwiftUI
 
 @main
@@ -21,13 +22,21 @@ struct PortfolioAppApp: App {
             } detail: {
                 DetailView()
             }
-                .environment(\.managedObjectContext, dataController.container.viewContext)
-                .environmentObject(dataController)
-                .onChange(of: scenePhase) { phase in
-                    if phase != .active {
-                        dataController.save()
-                    }
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+            .onChange(of: scenePhase) { phase in
+                if phase != .active {
+                    dataController.save()
                 }
+            }
+            .onContinueUserActivity(CSSearchableItemActionType, perform: loadSpotlightItem)
+        }
+    }
+    
+    func loadSpotlightItem(_ userActivity: NSUserActivity) {
+        if let uniqueIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String {
+            dataController.selectedIssue = dataController.issue(with: uniqueIdentifier)
+            dataController.selectedFilter = .all
         }
     }
 }
