8b0e563d92550858958e32ddf591a2663c7f11aa
ajtrumbly <ajtrumbly@hotmail.com>
2024-07-23 12:02:29 -0700

    Added Notifications for reminders in portfolio app

--- PortfolioApp/PortfolioApp/Extensions/CoreDataHelpers.swift
+++ PortfolioApp/PortfolioApp/Extensions/CoreDataHelpers.swift
@@ -53,6 +53,11 @@ extension Issue {
         issueCreationDate.formatted(date: .numeric, time: .omitted)
     }
     
+    var issueReminderTime: Date {
+        get { reminderTime ?? .now }
+        set { reminderTime = newValue }
+    }
+    
     static var example: Issue {
         let controller = DataController(inMemory: true)
         let viewContent = controller.container.viewContext
